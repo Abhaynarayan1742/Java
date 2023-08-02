2fad40e96af1e90b8d9dd93b95da45e8850dfac3
Abhaynarayan1742 <140948926+Abhaynarayan1742@users.noreply.github.com>
2023-08-02 10:17:39 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+//DECLERATRION INSIDE MAIN
+import java.util.*;  
+enum days {  
+  SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY  
+}  
+public class Main {  
+  public static void main(String[] args) {  
+    Set<days> set = EnumSet.of(days.TUESDAY, days.WEDNESDAY);  
+    Iterator<days> iter = set.iterator();  
+    while (iter.hasNext())  
+      System.out.println(iter.next());  
+  }  
+}  
